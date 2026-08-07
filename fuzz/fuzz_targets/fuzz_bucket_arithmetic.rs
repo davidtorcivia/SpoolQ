dc3702afbb5693de78132077335bc24288cc2fa1
David Torcivia <david.torcivia@gmail.com>
2026-08-06 23:21:34 -0400

    fix fuzz: bucket_arithmetic target use bytes-based input

--- fuzz/fuzz_targets/fuzz_bucket_arithmetic.rs
+++ fuzz/fuzz_targets/fuzz_bucket_arithmetic.rs
@@ -3,13 +3,16 @@
 
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use spoolq_math;
 
-fuzz_target!(|(timestamp, width): (u64, u64)| {
-    if width > 0 {
-        let _ = spoolq_math::bucket_number(timestamp, width);
-        let _ = spoolq_math::ceiling_bucket(timestamp, width);
-        let _ = spoolq_math::eligibility_bucket_and_ns(timestamp, width);
-        let _ = spoolq_math::bucket_end_ns(timestamp, width);
+fuzz_target!(|data: &[u8]| {
+    if data.len() >= 16 {
+        let timestamp = u64::from_be_bytes(data[0..8].try_into().unwrap());
+        let width = u64::from_be_bytes(data[8..16].try_into().unwrap());
+        if width > 0 {
+            let _ = spoolq_math::bucket_number(timestamp, width);
+            let _ = spoolq_math::ceiling_bucket(timestamp, width);
+            let _ = spoolq_math::eligibility_bucket_and_ns(timestamp, width);
+            let _ = spoolq_math::bucket_end_ns(timestamp, width);
+        }
     }
 });
